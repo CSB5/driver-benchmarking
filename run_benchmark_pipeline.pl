412f3c4596b32d9fadd95cd7994e04eaebd95098
chiakhb <chiakhb@gis.a-star.edu.sg>
2015-02-18 12:18:12 +0800

    FIX: Bugs introduced by addition of $software variable

--- run_benchmark_pipeline.pl
+++ run_benchmark_pipeline.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/perl
 
-use warnings;
-no warnings 'experimental';
+#use warnings;
+#no warnings 'experimental';
 use Config::Simple;
 use Getopt::Long;
 use POSIX 'strftime';
 use 5.010;
 
-my $version = "2.0.1";
+my $version = "2.0.2";
 my $date = strftime '%Y%m%d', localtime;
 my $runID = "${version}_${date}";
 
@@ -79,23 +79,22 @@ print "done.\n";
 
 ### Softwares/Packages ###
 # oncoIMPACT
-$software = "oncoIMPACT"; 
-if($config{'general.$software'}){
-	print "Running $software. Please wait...";
-
+if($config{'general.oncoIMPACT'}){	
+	print "Running oncoIMPACT. Please wait...";
+	
 	# Generate config file
-	generateConfig($software);
+	generateConfig("oncoIMPACT");
 	
 	
 	# Run oncoIMPACT
-	$command = "$qsub -l mem_free=$config{'cluster.mem'}G,h_rt=$runtime -pe OpenMP $config{'$software.numThreads'} -N $config{'general.disease'}_${software} -e $logsDir/$software.error.log -o $logsDir/$software.run.log $config{'$software.scriptsDir'}/oncoIMPACT.pl $analysisDir/${software}_$runID.cfg $config{'$software.subSample'}";
+	$command = "$qsub -l mem_free=$config{'cluster.mem'}G,h_rt=$runtime -pe OpenMP $config{'oncoIMPACT.numThreads'} -N $config{'general.disease'}_oncoIMPACT -e $logsDir/oncoIMPACT.error.log -o $logsDir/oncoIMPACT.run.log $config{'oncoIMPACT.scriptsDir'}/oncoIMPACT.pl $analysisDir/oncoIMPACT_$runID.cfg $config{'oncoIMPACT.subSample'}";
 	$command = $command . " 1" if ($flag_debug);
 	submit($command);
 	
 	# Parse oncoIMPACT output to standard format
 	$lastID = $queue[-1];
 	chomp($lastID);
-	$command = "$qsub -l mem_free=1G,h_rt=0:10:0 -pe OpenMP 1 -N $config{'general.disease'}_${software}_parseOutput -e $logsDir/${software}_parseOutput.error.log -o $logsDir/${software}_parseOutput.run.log -hold_jid $lastID $config{'$software.scriptsDir'}/parse_to_standard_output.pl --in $analysisDir/oncoIMPACT_analysis/GENE_LIST/ALTERATION.dat --out $resultsDir/$software.result";
+	$command = "$qsub -l mem_free=1G,h_rt=0:10:0 -pe OpenMP 1 -N $config{'general.disease'}_oncoIMPACT_parseOutput -e $logsDir/oncoIMPACT_parseOutput.error.log -o $logsDir/oncoIMPACT_parseOutput.run.log -hold_jid $lastID $config{'oncoIMPACT.scriptsDir'}/parse_to_standard_output.pl --in $analysisDir/oncoIMPACT_analysis/GENE_LIST/ALTERATION.dat --out $resultsDir/oncoIMPACT.result";
 	$command = $command . " --debug" if ($flag_debug);
 	submit($command);
 	
@@ -104,26 +103,25 @@ if($config{'general.$software'}){
 
 
 # DriverNet
-$software = "DriverNet"; 
-if($config{'general.$software'}){
-	print "Running $software. Please wait...";
+if($config{'general.DriverNet'}){
+	print "Running DriverNet. Please wait...";
 	
 	# Initialise folder
 	$outDir = "$analysisDir/DRIVERNET";
 	system("mkdir -p $outDir") unless (-d $outDir);
 	
 	# Generate config file
-	generateConfig($software);
+	generateConfig("DriverNet");
 	
 	# Run DriverNet
-	$command = "$qsub -l mem_free=$config{'cluster.mem'}G,h_rt=$runtime -pe OpenMP $config{'$software.numThreads'} -N $config{'general.disease'}_$software -e $logsDir/$software.error.log -o $logsDir/$software.run.log $config{'$software.scriptsDir'}/run_driver_net.pl --config $analysisDir/${software}_$runID.cfg";
+	$command = "$qsub -l mem_free=$config{'cluster.mem'}G,h_rt=$runtime -pe OpenMP $config{'DriverNet.numThreads'} -N $config{'general.disease'}_DriverNet -e $logsDir/DriverNet.error.log -o $logsDir/DriverNet.run.log $config{'DriverNet.scriptsDir'}/run_driver_net.pl --config $analysisDir/DriverNet_$runID.cfg";
 	$command = $command . " --debug" if ($flag_debug);
 	submit($command);
 	
 	# Parse DriverNet output to standard format
 	$lastID = $queue[-1];
 	chomp($lastID);
-	$command = "$qsub -l mem_free=1G,h_rt=0:10:0 -pe OpenMP 1 -N $config{'general.disease'}_${software}_parseOutput -e $logsDir/${software}_parseOutput.error.log -o $logsDir/${software}_parseOutput.run.log -hold_jid $lastID $config{'$software.scriptsDir'}/parse_to_standard_output.pl --in $outDir/res_driver_net.dat --out $resultsDir/$software.result";
+	$command = "$qsub -l mem_free=1G,h_rt=0:10:0 -pe OpenMP 1 -N $config{'general.disease'}_DriverNet_parseOutput -e $logsDir/DriverNet_parseOutput.error.log -o $logsDir/DriverNet_parseOutput.run.log -hold_jid $lastID $config{'DriverNet.scriptsDir'}/parse_to_standard_output.pl --in $outDir/res_driver_net.dat --out $resultsDir/DriverNet.result";
 	$command = $command . " --debug" if ($flag_debug);
 	submit($command);
 	
@@ -132,20 +130,19 @@ if($config{'general.$software'}){
 
 
 # MutSigCV
-$software = "MutSigCV"; 
-if($config{'general.$software'}){
-	print "Running $software. Please wait...";
+if($config{'general.MutSigCV'}){
+	print "Running MutSigCV. Please wait...";
 	
 	# Initialise folder	
 	$outDir = "$analysisDir/MUTSIGCV";
 	system("mkdir -p $outDir") unless (-d $outDir);
 	
 	# Generate config file
-	generateConfig($software);
+	generateConfig("MutSigCV");
 	
 	# Filter TCGA MAF file
-	if($config{'$software.flagFilter'}){
-		$command = "$qsub -l mem_free=$config{'cluster.mem'}G,h_rt=$runtime -pe OpenMP 1 -N $config{'general.disease'}_${software}_filterMAF -e $logsDir/${software}_filterMAF.error.log -o $logsDir/${software}_filterMAF.run.log $config{'$software.scriptsDir'}/filter_maf.pl --samples $config{'general.completeSamples'} --maf $config{'$software.maf'} --outDir $config{'$software.dataDir'}";
+	if($config{'MutSigCV.flagFilter'}){
+		$command = "$qsub -l mem_free=$config{'cluster.mem'}G,h_rt=$runtime -pe OpenMP 1 -N $config{'general.disease'}_MutSigCV_filterMAF -e $logsDir/MutSigCV_filterMAF.error.log -o $logsDir/MutSigCV_filterMAF.run.log $config{'MutSigCV.scriptsDir'}/filter_maf.pl --samples $config{'general.completeSamples'} --maf $config{'MutSigCV.maf'} --outDir $config{'MutSigCV.dataDir'}";
 		$command = $command . " --debug" if ($flag_debug);
 		submit($command);
 		
@@ -156,14 +153,14 @@ if($config{'general.$software'}){
 	}
 	
 	# Run MutSigCV
-	$command = "$qsub -l mem_free=$config{'cluster.mem'}G,h_rt=$runtime -pe OpenMP 1 -N $config{'general.disease'}_$software -e $logsDir/$software.error.log -o $logsDir/$software.run.log -hold_jid $lastID $config{'$software.scriptsDir'}/run_MutSigCV.pl --config $analysisDir/${software}_$runID.cfg'}";
+	$command = "$qsub -l mem_free=$config{'cluster.mem'}G,h_rt=$runtime -pe OpenMP 1 -N $config{'general.disease'}_MutSigCV -e $logsDir/MutSigCV.error.log -o $logsDir/MutSigCV.run.log -hold_jid $lastID $config{'MutSigCV.scriptsDir'}/run_MutSigCV.pl --config $analysisDir/MutSigCV_$runID.cfg'}";
 	$command = $command . " --debug" if ($flag_debug);
 	submit($command);
 	
 	# Parse MutSigCV output to standard format
 	$lastID = $queue[-1];
 	chomp($lastID);
-	$command = "$$qsub -l mem_free=1G,h_rt=0:10:0 -pe OpenMP 1 -N $config{'general.disease'}_${software}_parseOutput -e $logsDir/${software}_parseOutput.error.log -o $logsDir/${software}_parseOutput.run.log -hold_jid $lastID $config{'$software.scriptsDir'}/parse_to_standard_output.pl --in $outDir/$config{'general.disease'}.sig_genes.txt --out $resultsDir/$software.result";
+	$command = "$$qsub -l mem_free=1G,h_rt=0:10:0 -pe OpenMP 1 -N $config{'general.disease'}_MutSigCV_parseOutput -e $logsDir/MutSigCV_parseOutput.error.log -o $logsDir/MutSigCV_parseOutput.run.log -hold_jid $lastID $config{'MutSigCV.scriptsDir'}/parse_to_standard_output.pl --in $outDir/$config{'general.disease'}.sig_genes.txt --out $resultsDir/MutSigCV.result";
 	$command = $command . " --debug" if ($flag_debug);
 	submit($command);
 	
@@ -189,6 +186,7 @@ sub submit {
 sub generateConfig {
 	my $software = "@_";
 	open(OUT, "> $analysisDir/${software}_$runID.cfg");
+	print TRACE "[Config] Generating file: $analysisDir/${software}_$runID.cfg\n" if ($flag_debug);
 	
 	given($software){
 		when( 'oncoIMPACT'){
