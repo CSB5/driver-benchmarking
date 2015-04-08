d52d03f22a9a382bd5b653a239a1abf61cd05140
Burton Chia <chiakhb@gis.a-star.edu.sg>
2015-04-08 12:35:42 +0800

    New: Added MutationTaster Fix: Gene scores are averaged and reported Fix: Moved codes to sub routines

--- LJB/parse_to_standard_output.pl
+++ LJB/parse_to_standard_output.pl
@@ -2,6 +2,7 @@
 
 use warnings;
 use Getopt::Long;
+use List::Util qw(sum);
 
 my ($file_in, $outDir, $file_out, $flag_debug, $flag_help);
 
@@ -52,70 +53,195 @@ if ($flag_debug) {
 }
 
 
-my ($counter, @temp, $gene, $score, $type, $threshold, %reportedGenes);
+my ($counter, @temp, $gene, $score, $type, $threshold, %reportedGenes, $currentGene, @currentScore);
+
+sift();
+polyphen2();
+mutationAssessor();
+mutationTaster();
+
+######## sub routines ########
+sub mean {
+    return sum(@_)/@_;
+}
 
 # SIFT
-$counter = 1;
-$threshold = 0.05;
-%reportedGenes = ();
-$file_out = "$outDir/SIFT.result";
-open(IN, "cut -f 7,13 $file_in | tail -n+2 | sort -k2,2n |");
-open(OUT, "> $file_out");
-print OUT "Gene_name\tSample\tRank\tScore\tInfo\n";	# print header
-while(<IN>){
-	chomp(@temp = split(/\t/, $_));
-	$gene = $temp[0];
-	$score = $temp[1];
-	next if ($score eq "." || exists $reportedGenes{$gene});
-	last if($score > $threshold);
-	print OUT $gene . "\t" . "ALL" . "\t" . $counter . "\t" . $score . "\t" . "-" . "\n";
-	$reportedGenes{$gene} = "";
-	$counter++;
+sub sift {	
+	$threshold = 0.05;
+	$file_out = "$outDir/SIFT.temp";
+	open(OUT, "> $file_out");
+	open(IN, "cut -f 7,13 $file_in | tail -n+2 | sort -k1,1 |");
+	$currentGene = "";
+	@currentScore = ();
+	while(<IN>){
+		chomp(@temp = split(/\t/, $_));
+		$gene = $temp[0];
+		$score = $temp[1];
+		next if ($score eq "." || $score > $threshold);
+		if( $gene ne $currentGene && $currentGene ne ""){
+			print OUT $gene . "\t" . mean(@currentScore) . "\n";
+			$currentGene = "";
+			@currentScore = ();				
+		} else{
+			$currentGene = $gene if ($currentGene eq "");
+			push(@currentScore, $score);
+		}		
+	}
+	close(OUT);
+	close(IN);
+		
+	$counter = 1;
+	$file_out = "$outDir/SIFT.result";
+	open(IN, "sort -k2,2n $outDir/SIFT.temp |");
+	open(OUT, "> $file_out");
+	print OUT "Gene_name\tSample\tRank\tScore\tInfo\n";	# print header
+	while(<IN>){
+		chomp(@temp = split(/\t/, $_));
+		$gene = $temp[0];
+		$score = $temp[1];
+		print OUT $gene . "\t" . "ALL" . "\t" . $counter . "\t" . $score . "\t" . "-" . "\n";
+		$counter++;
+	}
+	close(OUT);
+	close(IN);
+	system("rm -f $outDir/SIFT.temp");
 }
-close(OUT);
-close(IN);
+
 
 # Polyphen2_HVAR_score
-$counter = 1;
-$threshold = 0.909; # stringent mode
-# $threshold = 0.446; # relaxed mode
-%reportedGenes = ();
-$file_out = "$outDir/PolyPhen2.result";
-open(IN, "cut -f 7,17 $file_in | tail -n+2 | sort -k2,2nr |");
-open(OUT, "> $file_out");
-print OUT "Gene_name\tSample\tRank\tScore\tInfo\n";	# print header
-while(<IN>){
-	chomp(@temp = split(/\t/, $_));
-	$gene = $temp[0];
-	$score = $temp[1];
-	next if ($score eq "." || exists $reportedGenes{$gene});
-	last if($score < $threshold);
-	print OUT $gene . "\t" . "ALL" . "\t" . $counter . "\t" . $score . "\t" . "-" . "\n";
-	$reportedGenes{$gene} = "";
-	$counter++;
+sub polyphen2 {
+	$threshold = 0.909; # stringent mode
+	# $threshold = 0.446; # relaxed mode
+	$file_out = "$outDir/PolyPhen2.temp";
+	open(OUT, "> $file_out");
+	open(IN, "cut -f 7,17 $file_in | tail -n+2 | sort -k1,1 |");
+	$currentGene = "";
+	@currentScore = ();
+	while(<IN>){
+		chomp(@temp = split(/\t/, $_));
+		$gene = $temp[0];
+		$score = $temp[1];
+		next if ($score eq "." || $score < $threshold);
+		if( $gene ne $currentGene && $currentGene ne ""){
+			print OUT $gene . "\t" . mean(@currentScore) . "\n";
+			$currentGene = "";
+			@currentScore = ();				
+		} else{
+			$currentGene = $gene if ($currentGene eq "");
+			push(@currentScore, $score);
+		}		
+	}
+	close(OUT);
+	close(IN);
+	
+	$counter = 1;
+	$file_out = "$outDir/PolyPhen2.result";
+	open(IN, "sort -k2,2nr $outDir/PolyPhen2.temp |");
+	open(OUT, "> $file_out");
+	print OUT "Gene_name\tSample\tRank\tScore\tInfo\n";	# print header
+	while(<IN>){
+		chomp(@temp = split(/\t/, $_));
+		$gene = $temp[0];
+		$score = $temp[1];
+		print OUT $gene . "\t" . "ALL" . "\t" . $counter . "\t" . $score . "\t" . "-" . "\n";
+		$counter++;
+	}
+	close(OUT);
+	close(IN);
+	system("rm -f $outDir/PolyPhen2.temp");
 }
-close(OUT);
-close(IN);
+
 
 # MutationAssessor
-$counter = 1;
-%reportedGenes = ();
-$file_out = "$outDir/MutationAssessor.result";
-open(IN, "cut -f 7,23,24 $file_in | tail -n+2 | sort -k2,2nr |");
-open(OUT, "> $file_out");
-print OUT "Gene_name\tSample\tRank\tScore\tInfo\n";	# print header
-while(<IN>){
-	chomp(@temp = split(/\t/, $_));
-	$gene = $temp[0];
-	$score = $temp[1];
-	$type = $temp[2];
-	next if ($score eq "." || exists $reportedGenes{$gene});
-	next unless ($type eq "H"	# stringent mode
-				 #|| $type eq "M" # relaxed mode
-	);
-	print OUT $gene . "\t" . "ALL" . "\t" . $counter . "\t" . $score . "\t" . "-" . "\n";
-	$reportedGenes{$gene} = "";
-	$counter++;
+sub mutationAssessor {
+	$file_out = "$outDir/MutationAssessor.temp";
+	open(OUT, "> $file_out");
+	open(IN, "cut -f 7,23,24 $file_in | tail -n+2 | sort -k1,1 |");
+	$currentGene = "";
+	@currentScore = ();
+	while(<IN>){
+		chomp(@temp = split(/\t/, $_));
+		$gene = $temp[0];
+		$score = $temp[1];		
+		$type = $temp[2];
+		next if ($score eq ".");
+		next unless ($type eq "H"	# stringent mode
+					 #|| $type eq "M" # relaxed mode
+		);
+		if( $gene ne $currentGene && $currentGene ne ""){
+			print OUT $gene . "\t" . mean(@currentScore) . "\t". $type . "\n";
+			$currentGene = "";
+			@currentScore = ();				
+		} else{
+			$currentGene = $gene if ($currentGene eq "");
+			push(@currentScore, $score);
+		}		
+	}
+	close(OUT);
+	close(IN);
+	
+	$counter = 1;
+	$file_out = "$outDir/MutationAssessor.result";
+	open(IN, "sort -k2,2nr $outDir/MutationAssessor.temp | ");
+	open(OUT, "> $file_out");
+	print OUT "Gene_name\tSample\tRank\tScore\tInfo\n";	# print header
+	while(<IN>){
+		chomp(@temp = split(/\t/, $_));
+		$gene = $temp[0];
+		$score = $temp[1];
+		$type = $temp[2];
+		next if ($score eq ".");
+		print OUT $gene . "\t" . "ALL" . "\t" . $counter . "\t" . $score . "\t" . "-" . "\n";
+		$reportedGenes{$gene} = "";
+		$counter++;
+	}
+	close(OUT);
+	close(IN);
+	system("rm -f $outDir/MutationAssessor.temp");
 }
-close(OUT);
-close(IN);
+
+
+# MutationTaster
+sub mutationTaster {
+	$file_out = "$outDir/MutationTaster.temp";
+	open(OUT, "> $file_out");
+	open(IN, "cut -f 7,21,22 $file_in | tail -n+2 | sort -k1,1 |");
+	$currentGene = "";
+	@currentScore = ();
+	while(<IN>){
+		chomp(@temp = split(/\t/, $_));
+		$gene = $temp[0];
+		$score = $temp[1];		
+		$type = $temp[2];
+		next if ($score eq ".");
+		next unless ($type eq "A" || $type eq "D"	# stringent mode);
+		if( $gene ne $currentGene && $currentGene ne ""){
+			print OUT $gene . "\t" . mean(@currentScore) . "\t". $type . "\n";
+			$currentGene = "";
+			@currentScore = ();				
+		} else{
+			$currentGene = $gene if ($currentGene eq "");
+			push(@currentScore, $score);
+		}		
+	}
+	close(OUT);
+	close(IN);
+	
+	$counter = 1;
+	$file_out = "$outDir/MutationTaster.result";
+	open(IN, "sort -k2,2nr $outDir/MutationTaster.temp |");
+	open(OUT, "> $file_out");
+	print OUT "Gene_name\tSample\tRank\tScore\tInfo\n";	# print header
+	while(<IN>){
+		chomp(@temp = split(/\t/, $_));
+		$gene = $temp[0];
+		$score = $temp[1];
+		$type = $temp[2];
+		print OUT $gene . "\t" . "ALL" . "\t" . $counter . "\t" . $score . "\t" . "-" . "\n";
+		$reportedGenes{$gene} = "";
+		$counter++;
+	}
+	close(OUT);
+	close(IN);
+	system("rm -f $outDir/MutationTaster.temp");
+}
