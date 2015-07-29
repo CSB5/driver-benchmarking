09494d4168df340906c7f5eed156fd60283a378a
Burton Chia <chiakhb@gis.a-star.edu.sg>
2015-07-29 17:38:16 +0800

    Fix: Bug fixes. Added additional filtering to be consistent with paper.

--- ONCODRIVEFM/run_OncodriveFM.pl
+++ ONCODRIVEFM/run_OncodriveFM.pl
@@ -54,44 +54,48 @@ $out_dir = $config{'default.outDir'};
 
 
 # Generate OncodriveFM input
-if (index($annot_file, "hg19") != -1) {
-	# hg19
-	my %score_col = (
-	    "SIFT_score", 12,
-	    "Polyphen2_HDIV_score", 14,
-	    "Polyphen2_HVAR_score", 16,
-	    "LRT_score", 18,
-	    "MutationTaster_score", 20,
-	    "MutationAssessor_score", 22,
-	    "FATHMM_score", 24,
-	    "RadialSVM_score", 26,
-	    "LR_score", 28,
-	    "VEST3_score", 30,
+#The distinction beween hg19 and hg18 is not relevant as we add the additional column to the annovar file
+#if (index($annot_file, "hg19") != -1) {
+# hg19
+my %score_col = (
+    "SIFT_score", 12,
+    "Polyphen2_HDIV_score", 14,
+    "Polyphen2_HVAR_score", 16,
+    "LRT_score", 18,
+    "MutationTaster_score", 20,
+    "MutationAssessor_score", 22,
+    "FATHMM_score", 24,
+    "RadialSVM_score", 26,
+    "LR_score", 28,
+    "VEST3_score", 30,
     );    
-} else{
+#} 
+
+#else{
 	# hg18
-	my %score_col = (
-	    "SIFT_score", 10,
-	    "Polyphen2_HDIV_score", 12,
-	    "Polyphen2_HVAR_score", 14,
-	    "LRT_score", 16,
-	    "MutationTaster_score", 18,
-	    "MutationAssessor_score", 20,
-	    "FATHMM_score", 22,
-	    "RadialSVM_score", 24,
-	    "LR_score", 26,
-	    "VEST3_score", 28,
-    );   
-}
+#my %score_col = (
+#	    "SIFT_score", 10,
+#	    "Polyphen2_HDIV_score", 12,
+#	    "Polyphen2_HVAR_score", 14,
+#	    "LRT_score", 16,
+#	    "MutationTaster_score", 18,
+#	    "MutationAssessor_score", 20,
+#	    "FATHMM_score", 22,
+#	    "RadialSVM_score", 24,
+#	    "LR_score", 26,
+#	    "VEST3_score", 28,
+ #   );   
+#}
 
 
 
 my %score_used = (
     "SIFT_score", "SIFT",
     "Polyphen2_HVAR_score", "PPH2",
-    "MutationAssessor_score", "MA"
+    "MutationAssessor_score", "MA" 
     );
-my @score_order = keys %score_used;
+#Fixed score order needed for the fixed score
+my @score_order = ("SIFT_score", "Polyphen2_HVAR_score", "MutationAssessor_score");
 
 $header = "SAMPLE\tGENE";
 foreach $s (@score_order){
@@ -101,21 +105,45 @@ open(OUT, ">$out_dir/OncodriveFM.txt");
 print OUT $header."\n";
 
 open(FILE, $annot_file);
+my @res_score = ();
 while(<FILE>){
     chop $_;
     @line = split(/\t/, $_);
     
     next if($line[0] eq "Chr");
 
+    @all_effect = split(/\;/, $line[8]);
+    @all_effect = ($line[8]) if(@all_effect == 0);
+    $eff = $all_effect[0];
 
     $all_define = 1;
-    my @res_score = ();
-    for($i = 0; $i < @score_order; $i++){
-	$s = $score_order[$i];
-	$s_val = $line[$score_col{$s}];
-	push(@res_score, $s_val);
-	if($s_val eq "."){
-	    $all_define = 0;
+
+    #For non synonymous mutations we get the score of each FM methods
+    if($eff eq "nonsynonymous SNV"){
+	@res_score = ();
+	for(my $i = 0; $i < @score_order; $i++){
+	    $s = $score_order[$i];
+	    $s_val = $line[$score_col{$s}];
+	    push(@res_score, $s_val);
+	    if($s_val eq "."){
+		$all_define = 0;
+	    }
+	}
+    }
+    else{
+	#Mutation known to have an effect on the protein
+	if($eff eq "frameshift substitution" || $eff eq "stopgain" || $eff eq "stoploss"){
+	    @res_score = (0, 1, 3.5);
+	}
+	else{
+	    #Synonymous mutations
+	    if($eff eq "synonymous SNV"){
+		@res_score = (1, 0, -2);
+	    }
+	    #All other mutations are unused
+	    else{
+		$all_define = 0; 
+	    }
 	}
     }
     
